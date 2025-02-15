473caff1ebbc74ddf13905a5f55f1be47a54795f
Mary <Anymail@mail.com>
2025-02-15 09:52:17 +0300

    доп2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -24,75 +24,73 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+
+
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt("Click", 0);
+            TextView txt = findViewById(R.id.txtCounter);
+            txt.setText(count.toString());
+        }
     }
+
     @Override
-    protected void onStart()
-    {
+    protected void onStart() {
         super.onStart();
         Log.d("Instance", "onStart");
-        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
         Log.d("Instance", "onResume");
-        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
     }
 
-    //изм
-
     @Override
-    protected void onPause()
-    {
+    protected void onPause() {
         super.onPause();
         Log.d("Instance", "onPause");
-        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
     }
 
     @Override
-    protected void onStop()
-    {
+    protected void onStop() {
         super.onStop();
         Log.d("Instance", "onStop");
-        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
     }
 
     @Override
-    protected void onDestroy()
-    {
+    protected void onDestroy() {
         super.onDestroy();
         Log.d("Instance", "onDestroy");
-        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
     }
+
     @Override
-    protected void onSaveInstanceState(Bundle outState)
-    {
+    protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("Click", count);
         Log.d("Instance", "onSave");
-        Toast.makeText(this, "onSave", Toast.LENGTH_LONG).show();
+    }
 
-    }
-    protected void onRestoreInstanceState(Bundle SavedState){
-        super.onRestoreInstanceState(SavedState);
-        if(SavedState != null && SavedState.containsKey("Сlick")){
-            count = SavedState.getInt("Сlick");
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null && savedInstanceState.containsKey("Click")) {
+            count = savedInstanceState.getInt("Click");
+            TextView txt = findViewById(R.id.txtCounter);
+            txt.setText(count.toString());
         }
-        Log.d("Instance","get");
-        Toast.makeText(this, "get",Toast.LENGTH_LONG).show();
+        Log.d("Instance", "get");
     }
+
     public void onClickButton(View view) {
         count++;
-        TextView txt= findViewById(R.id.txtCounter);
+        TextView txt = findViewById(R.id.txtCounter);
         txt.setText(count.toString());
     }
 
     public void onClickButton1(View view) {
         count += 2;
-        TextView txt= findViewById(R.id.txtCounter);
+        TextView txt = findViewById(R.id.txtCounter);
         txt.setText(count.toString());
     }
 }
