98f9d4db20b7b4ced8b16b62b43e779a9ab586e8
Mary <Anymail@mail.com>
2025-02-15 10:01:11 +0300

    доп2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,6 +12,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.BreakIterator;
+
 public class MainActivity extends AppCompatActivity {
     private Integer count = 0;
     @Override
